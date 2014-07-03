ba1dd629d4492b5e2c8189b25acbc92ad9ee730f
newOcean <erduhai@qq.com>
2014-07-03 11:14:53 +0800

    first commit

--- AppRTCDemo/APPRTCViewController.m
+++ AppRTCDemo/APPRTCViewController.m
@@ -14,10 +14,19 @@
 
 @implementation APPRTCViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
---
+++ README.md
@@ -0,0 +1,3 @@
+2014.7.1
+The latest webrtc AppRTCDemo,I compile the webrtc project,and run it on ios device.
+Now, I upload the demo and librarys,for anyone reference.
